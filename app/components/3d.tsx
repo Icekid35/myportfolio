942a5edbeb272739426666a22e3907a4429a66fd
icekid35 <bellohabib682@gmail.com>
2025-03-23 15:27:52 +0100

    updated model

--- app/components/3d.tsx
+++ app/components/3d.tsx
@@ -1,34 +1,7 @@
 "use client"
 import React from "react";
-import "@google/model-viewer/lib/model-viewer";
+import  "@google/model-viewer/lib/model-viewer";
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      "model-viewer": ModelViewerJSX &
-        React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    }
-  }
-}
-
-interface ModelViewerJSX {
-  src: string;
-  poster?: string;
-  iosSrc?: string;
-  seamlessPoster?: boolean;
-  autoplay?: boolean;
-  environmentImage?: string;
-  exposure?: string;
-  interactionPromptThreshold?: string;
-  shadowIntensity?: string;
-  ar?: boolean;
-  arModes?: string;
-  autoRotate?: boolean;
-  cameraControls?: boolean;
-  cameraOrbit?: string;
-  alt?: string;
-  sx?: any;
-}
 const Model = () => {
 
   const glbSrc = "/models/avater.glb";
@@ -41,6 +14,7 @@ const Model = () => {
     <button slot="ar-button" id="ar-button">
         View in your space
     </button>
+    
     <div id="ar-prompt"  >
         <img src="https://modelviewer.dev/shared-assets/icons/hand.png"    />
     </div>
