93cab49eca782a0aad6702afdf0493523b963bcd
icekid35 <bellohabib682@gmail.com>
2025-03-23 16:55:08 +0100

    removed controls

--- app/components/3d.tsx
+++ app/components/3d.tsx
@@ -7,7 +7,9 @@ const Model = () => {
   const glbSrc = "/models/avater.glb";
 
   return (
-   <model-viewer src={glbSrc} ar ar-modes="webxr scene-viewer quick-look" className=" h-full w-full " camera-controls tone-mapping="neutral" poster="/images/poster.webp" shadow-intensity="1" auto-rotate>
+   <model-viewer  src={glbSrc} ar ar-modes="webxr scene-viewer quick-look" className=" h-full w-full " camera-controls tone-mapping="neutral" poster="/images/poster.webp" style={{
+    backgroundColor:"var(--color-body)"
+   }} shadow-intensity="1" auto-rotate>
 
 </model-viewer>
 
