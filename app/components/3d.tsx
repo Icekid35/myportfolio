cb9c2bd54ae8f0c62888d4fb908d0cd27df7635b
icekid35 <bellohabib682@gmail.com>
2025-03-23 16:46:04 +0100

    removed controls

--- app/components/3d.tsx
+++ app/components/3d.tsx
@@ -8,16 +8,7 @@ const Model = () => {
 
   return (
    <model-viewer src={glbSrc} ar ar-modes="webxr scene-viewer quick-look" className=" h-full w-full " camera-controls tone-mapping="neutral" poster="/images/poster.webp" shadow-intensity="1" auto-rotate>
-    <div className="progress-bar hide" slot="progress-bar">
-        <div className="update-bar"></div>
-    </div>
-    <button slot="ar-button" id="ar-button">
-        View in your space
-    </button>
-    
-    <div id="ar-prompt"  >
-        <img src="https://modelviewer.dev/shared-assets/icons/hand.png"    />
-    </div>
+
 </model-viewer>
 
   );
