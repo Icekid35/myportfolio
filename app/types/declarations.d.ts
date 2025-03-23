6359999104ea153b6bc6be8875798412aaff468e
Bello Habeeb <81009270+Icekid35@users.noreply.github.com>
2025-03-23 15:52:38 +0100

    Update declarations.d.ts

--- app/types/declarations.d.ts
+++ app/types/declarations.d.ts
@@ -1,13 +1,15 @@
-import React from "react";
-import { ModelViewerAttributes } from "@google/model-viewer";
+if (typeof self !== "undefined") {
+  import React from "react";
+  import { ModelViewerAttributes } from "@google/model-viewer";
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      "model-viewer": React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement> & ModelViewerAttributes,
-        HTMLElement
-      >;
+  declare module "react" {
+    namespace JSX {
+      interface IntrinsicElements {
+        "model-viewer": React.DetailedHTMLProps<
+          React.HTMLAttributes<HTMLElement> & ModelViewerAttributes,
+          HTMLElement
+        >;
+      }
     }
   }
 }
